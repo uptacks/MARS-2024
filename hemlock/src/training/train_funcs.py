3576b1a0cb4829610fbc60df91896453a12d7781
uptacks <uptack@protonmail.com>
2024-02-18 17:17:49 +0000

    added llc estimation

--- hemlock/src/training/train_funcs.py
+++ hemlock/src/training/train_funcs.py
@@ -1,3 +1,5 @@
+import torch
+
 def train_clean_model(model, criterion, optimizer, dataloader, device, num_epochs=10):
     for epoch in range(num_epochs):
         model.train()
@@ -19,3 +21,40 @@ def train_clean_model(model, criterion, optimizer, dataloader, device, num_epoch
         print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
 
     return model
+
+def single_epoch(model, set, criterion, optimizer, dataloader, device):
+    # Set is train/val. Will change the name to something more meaningful.
+
+    if set == "train":
+        model.train()
+        running_loss = 0.0
+
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        return running_loss / len(dataloader)
+    if set == "test":
+        model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for inputs, labels in dataloader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
+        return running_loss / len(dataloader)
+ 
+
+
+
+
+
